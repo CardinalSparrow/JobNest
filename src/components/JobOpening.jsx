18b801cc616a9b6272ed957b263270cced6184a4
Cardinalsparrow <cardinalsparrow@gmail.com>
2025-01-10 10:15:40 +0100

    .

--- src/components/JobOpening.jsx
+++ src/components/JobOpening.jsx
@@ -1,42 +1,46 @@
 import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
-  faBookmark,
-  faEnvelope,
-  faLocationPinLock,
+	faBookmark,
+	faEnvelope,
+	faLocationPinLock,
 } from "@fortawesome/free-solid-svg-icons";
 export default function JobOpening() {
-  return (
-    <div className="w-[300px] h-[150px] mb-6 flex flex-col justify-between p-[5px] bg-white">
-      <div className="flex justify-between">
-        <div className={`flex justify-center items-center bg-background2-100 w-[68px] h-[45px]`}>
-          <FontAwesomeIcon icon={faEnvelope} size="2x" />
-        </div>
-        <div className={"flex flex-col justify-between w-[70%]"}>
-          <div className="text-primary font-[600]">Senior Developer</div>
-          <div className={`flex justify-between text-grey text-sm`}>
-            <span>
-              <FontAwesomeIcon icon={faLocationPinLock} color="#000" /> Lagos
-            </span>
-            <span>Remote</span>
-            <span>Full time</span>
-          </div>
-        </div>
-        <div>
-          <FontAwesomeIcon icon={faBookmark} size="" />
-        </div>
-      </div>
-      <div className={`text-grey text-sm flex justify-between`}>
-        <div>Software Engineering</div>
-        <div>20 Applicants</div>
-      </div>
-      <div className={`text-grey text-sm flex`}>
-        {" "}
-        <div>Posted by Me</div>
-      </div>
-      <div className={`flex justify-end`}>
-        <button className="bg-secondary text-white p-1 text-sm font-medium rounded-md">Apply Now!</button>
-      </div>
-    </div>
-  );
+	return (
+		<div className="w-[300px] h-[150px] mb-6 flex flex-col justify-between p-[5px] bg-white">
+			<div className="flex justify-between">
+				<div
+					className={`flex justify-center items-center bg-background2-100 w-[68px] h-[45px]`}
+				>
+					<FontAwesomeIcon icon={faEnvelope} size="2x" />
+				</div>
+				<div className={"flex flex-col justify-between w-[70%]"}>
+					<div className="text-primary font-[600]">Senior Developer</div>
+					<div className={`flex justify-between text-grey text-sm`}>
+						<span>
+							<FontAwesomeIcon icon={faLocationPinLock} color="#000" /> Lagos
+						</span>
+						<span>Remote</span>
+						<span>Full time</span>
+					</div>
+				</div>
+				<div>
+					<FontAwesomeIcon icon={faBookmark} size="" />
+				</div>
+			</div>
+			<div className={`text-grey text-sm flex justify-between`}>
+				<div>Software Engineering</div>
+				<div>20 Applicants</div>
+			</div>
+			<div className={`text-grey text-sm flex`}>
+				{" "}
+				<div>Posted by Me</div>
+			</div>
+			<div className={`flex justify-end`}>
+				<button className="bg-secondary text-white p-1 text-sm font-medium rounded-md">
+					Apply Now!
+				</button>
+			</div>
+		</div>
+	);
 }
