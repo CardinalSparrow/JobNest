b88750e3b7fd575551910d21ef4fe004133560d0
Cardinalsparrow <cardinalsparrow@gmail.com>
2025-01-18 16:25:29 +0100

    Working on the profile page

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Placepic from "../images/fortune-vieyra-xdmtQVVKibU-unsplash.png";
+import { ProfileFormItem } from "../components/ResuableComponents";
 
 const Profile = () => {
 	return (
@@ -14,18 +15,55 @@ const Profile = () => {
 					<p>Qualification</p>
 				</div>
 			</div>
-			<div className="flex flex-col py-3 px-8 border border-black w-full relative rounded-[40px] gap-2">
-				<label htmlFor="about" className="font-semibold">
-					About Me
-				</label>
-				<input
-					className="h-full  outline-none py-3"
-					type="text"
-					name="about"
-					placeholder="Give a short overview of your career history and skills."
-				/>
-				<button className=" absolute right-12 bottom-1">Add</button>
-			</div>
+			<ProfileFormItem
+				label={"About Me"}
+				placeholder={"Give a short overview of your career history and skills."}
+				button={"Add"}
+			/>
+			<ProfileFormItem
+				label={"Exerience"}
+				placeholder={
+					"Include your work experience, such as internships, part-time roles, or long-term specialized positions"
+				}
+				button={"Add"}
+			/>
+			<ProfileFormItem
+				label={"Qualification"}
+				placeholder={"List your qualifications here."}
+				button={"Add"}
+			/>
+			<ProfileFormItem
+				label={"Certification"}
+				placeholder={
+					"Received any certificates? Show them off to help enrich your career profile."
+				}
+				button={"Edit"}
+			/>
+			<ProfileFormItem
+				label={"Skills"}
+				placeholder={"What are your areas of expertise?"}
+				button={"Edit"}
+			/>
+			<ProfileFormItem
+				label={"Language Skills"}
+				placeholder={
+					"Let your employers know if you speak more than one language."
+				}
+				button={"Add"}
+			/>
+			<ProfileFormItem
+				label={"Cover Letter"}
+				placeholder={
+					"Showcase your experience, skills, and areas of expertise, emphasizing what you can contribute to the company and why you are the ideal candidate for the role. "
+				}
+				button={"Add"}
+			/>
+			<ProfileFormItem label={"Curriculum Vitae"} button={"Upload"} />
+			<ProfileFormItem
+				label={"Portfolio"}
+				placeholder={"Add links to your online projects and portfolios."}
+				button={"Add"}
+			/>
 		</section>
 	);
 };
